c93d62d5a5b801f4b4ddf31054080f6edbd3c7a3
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-26 21:53:36 -0300

    add json server

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
  <!-- Content -->
- <div class="px-3 py-10 md:px-10">
+    <div class="px-3 py-10 md:px-10">
         <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
 
             <!-- Todo spinner -->
