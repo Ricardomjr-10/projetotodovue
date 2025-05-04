c08d3188609b6a486a9da2741f2ff61a82f8a274
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-03 21:01:55 -0300

    atualizando todos

--- src/store/index.js
+++ src/store/index.js
@@ -10,28 +10,35 @@ export default createStore({
       state.todos = payload
     },
     storeTodo(state, payload) {
-      state.todos.push(payload)
+      const index = state.todos.findIndex(todo => todo.id === payload.id)
+      if (index >= 0) {
+        state.todos.splice(index, 1, payload)
+      } else {
+        state.todos.push(payload)
+      }
     },
   },
   actions: {
-    getTodos({commit}) {
+    getTodos({ commit }) {
       return new Promise((resolve) => {
         setTimeout(() => {
           return axios.get('http://localhost:3000/todos')
-          .then((response) => {
-            commit('storeTodos', response.data)
-            resolve()
-          })
-        },1000)
+            .then((response) => {
+              commit('storeTodos', response.data)
+              resolve()
+            })
+        }, 1000)
       })
     },
-    addTodo({commit}, data) {
-     return axios.post('http://localhost:3000/todos', data).then((response) => {
-        commit('storeTodo',response.data)
+    addTodo({ commit }, data) {
+      return axios.post('http://localhost:3000/todos', data).then((response) => {
+        commit('storeTodo', response.data)
       })
     },
-    updateTodo(context, {id, data}) {
-      return axios.put(`http://localhost:3000/todos/${id}`, data)
+    updateTodo({ commit }, { id, data }) {
+      return axios.put(`http://localhost:3000/todos/${id}`, data).then((response) => {
+        commit('storeTodo', response.data)
+      })
     }
   },
   getters: {
